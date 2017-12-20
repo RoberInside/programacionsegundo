8155479afb6efac8b32bdd464acc16202821c13a
borjacano97 <borcano@ucm.es>
2017-12-20 16:22:37 +0100

    Añadido PacmanFile

---
+++ HolaSDL/PacmanFile.h
@@ -0,0 +1,61 @@
+#ifndef _H_PACMAN_FILE_H_
+#define _H_PACMAN_FILE_H_
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class PacmanFile
+{
+public:
+	struct Par {
+		int x, y;
+	};
+	struct GameData
+	{
+		int level, score;
+	};
+	struct MapData
+	{
+		int rows, cols;
+		vector<int> map;
+	};
+	struct GhostData
+	{
+		int type, age;
+		Par pos, posini, dir;
+	};
+	struct GhostsData {
+		int numGhosts;
+		vector<GhostData> ghosts;
+	};
+	struct PacmanData
+	{
+		int energy, lifes;
+		Par pos, posini, dir;
+	};
+public:
+
+	PacmanFile(string fileName);
+	~PacmanFile();
+
+	inline GameData*	getGameData()		 { return &gameData;				};
+	inline MapData*		getMapData()		 { return &mapData;					};
+	inline GhostsData*	getGhostsData()		 { return &ghostsData;				};
+	inline PacmanData*	getPacmanData()		 { return &pacmanData;				};
+	inline GhostData*	getGhostData(int id) { return &ghostsData.ghosts[id];	};
+
+
+private:
+	GameData gameData;
+	MapData mapData;
+	GhostsData ghostsData;
+	PacmanData pacmanData;
+
+
+};
+#endif // !_H_PACMAN_FILE_H_
+
